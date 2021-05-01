d285e67b1943869b02d995e81f19531dcb5ba1e6
aadityajo <aadityajoshi2699@gmail.com>
2021-05-02 01:46:00 +0530

    heading for music player

--- src/components/landing/Skills/index.jsx
+++ src/components/landing/Skills/index.jsx
@@ -1,17 +1,35 @@
-import React, { useContext } from 'react';
+import React, { useContext,useEffect } from 'react';
 import AnchorLink from 'react-anchor-link-smooth-scroll';
 import { ThemeContext } from 'providers/ThemeProvider';
 import { Container, Button } from 'components/common';
 import dev from 'assets/illustrations/skills.svg';
 import { Wrapper, SkillsWrapper, Details, Thumbnail } from './styles';
+import "./index.css"
 
 export const Skills = () => {
   const { theme } = useContext(ThemeContext);
 
+
   return (
-    <div id="tunes" style={{paddingLeft:"20px"}} >
-    <iframe src="https://open.spotify.com/embed/playlist/5SMjI3BRUfxfOPpmun0p7s" width="100%" height="700" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
-    </div>
-  
+    <>
+    <Wrapper id="about">
+      <SkillsWrapper as={Container}>
+        <Details theme={theme}>
+          <center><h1>Recommended Tunes</h1></center>
+          <center>
+          <p>
+           Music that inspires us
+          </p>
+          </center>
+
+        </Details>
+      </SkillsWrapper>
+    </Wrapper>
+    <div  style={{backgroundColor: "rgb(243, 244, 249)"}}>      
+      <div  id="tunes" style={{paddingLeft:"20px",display:"flex",justifyContent:"center"}} >
+      <iframe className="playerVal" id="myFrame" src="https://open.spotify.com/embed/playlist/5SMjI3BRUfxfOPpmun0p7s" width="95%" height="700" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+      </div>
+    </div>  
+    </>
   );
 };
